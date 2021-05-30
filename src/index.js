a97eda01d891f5d7e331fdebbbb49ddea9740bb6
Luis Martinez <luis_mmartinez@live.com>
2021-05-30 15:29:47 -0400

    commit tree b1466bb5f320c138bc22db5837d4fe267ea57d28

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function fetchDogs() {
+  return fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => renderDogs(json))
+}
+
+function renderDogs() {
+
+}
+
+document.addEventListener("DOMContentLoeaded", function() {
+  fetchDogs();
+})
